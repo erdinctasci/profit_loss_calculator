4acf59ffa0fbe8045b80343544c4016519f72d0c
Erdinç <erdinc.tasci@yandex.com>
2024-03-14 14:36:07 +0300

    Percentage growth calculations have been added

--- profis_loss_calculator.R
+++ profis_loss_calculator.R
@@ -6,13 +6,19 @@ ui <- fluidPage(
   titlePanel("Kaldıraçlı İşlem Kar/Zarar Hesaplama"),
   sidebarLayout(
     sidebarPanel(
+      checkboxInput("hesaplamaTipi", "Yüzdesel Büyüme ile Hesapla", value = FALSE),
       numericInput("girisMiktari", "Pozisyona Giriş Miktarı (Dolar):", value = 10),
       numericInput("kaldirac", "Kaldıraç Oranı:", value = 10),
-      numericInput("alisFiyati", "Alış Fiyatı:", value = 0),
-      numericInput("satisFiyati", "Satış Fiyatı:", value = 0),
-      actionButton("hesapla", "Hesapla", 
-                   style = "border: 1px solid white; color: white; background-color: #337ab7; padding: 5px 10px; border-radius: 4px; 
-                      hover: background-color: #286090;")
+      conditionalPanel(
+        condition = "input.hesaplamaTipi == false",
+        numericInput("alisFiyati", "Alış Fiyatı:", value = 0),
+        numericInput("satisFiyati", "Satış Fiyatı:", value = 0)
+      ),
+      conditionalPanel(
+        condition = "input.hesaplamaTipi == true",
+        numericInput("yuzdeDegisim", "Yüzdesel Büyüme (%):", value = 0)
+      ),
+      actionButton("hesapla", "Hesapla", style = "color: white; background-color: #337ab7; padding: 5px 10px; border-radius: 4px;")
     ),
     mainPanel(
       uiOutput("sonuc")
@@ -26,9 +32,13 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   observeEvent(input$hesapla, {
-    toplamPozisyon <- input$girisMiktari * input$kaldirac
-    karZarar <- (input$satisFiyati - input$alisFiyati) * (toplamPozisyon / input$alisFiyati)
-    
+    if (!input$hesaplamaTipi) {  
+      toplamPozisyon <- input$girisMiktari * input$kaldirac
+      karZarar <- (input$satisFiyati - input$alisFiyati) * toplamPozisyon / input$alisFiyati
+    } else {  
+      toplamPozisyon <- input$girisMiktari * input$kaldirac
+      karZarar <- input$yuzdeDegisim / 100 * toplamPozisyon
+    }
     
     output$sonuc <- renderUI({
       if (karZarar >= 0) {
@@ -38,12 +48,11 @@ server <- function(input, output) {
       }
       
       tags$div(
-        tags$span(style = paste("color:", color, "; font-size: 16px;"), "Kar/Zarar:"),
+        tags$span(style = paste("color:", color, "; font-size: 16px;"), "Kar/Zarar: "),
         tags$div(style = paste("color:", color, "; font-size: 24px;"), paste("$", formatC(karZarar, format = "f", digits = 2)))
       )
     })
   })
 }
-
 
 shinyApp(ui = ui, server = server)
